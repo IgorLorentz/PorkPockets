c39bae0991dec87c1985481e03eb1bf8f0df85a0
Mannelz <emannuelm29@gmail.com>
2023-11-19 11:10:44 -0300

    create routes

---
+++ pork_pockets_app/lib/routes.dart
@@ -0,0 +1,8 @@
+class Routes 
+{
+  static const HOME = '/';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const METAS = '/metas';
+  static const NEW_METAS = '/new-metas';
+}
