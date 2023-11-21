8a3aabfd63889b59b71441fd9570594e263d5c5f
Mannelz <emannuelm29@gmail.com>
2023-11-21 15:42:02 -0300

    update color

--- pork_pockets_app/lib/util/color_util.dart
+++ pork_pockets_app/lib/util/color_util.dart
@@ -5,7 +5,8 @@ class Paleta
   static Color rosa = const Color.fromRGBO(242, 102, 139, 1);
   static Color verde = const Color.fromRGBO(3, 166, 136, 1);
   static Color azul = const Color.fromRGBO(4, 182, 202, 1);
-  static Color azulEscuro = const Color.fromRGBO(2, 94, 15, 1);
+  static Color azulEscuro = const Color.fromRGBO(2, 94, 115, 1);
   static Color azulEscurao = const Color.fromRGBO(1, 31, 38, 1); 
   static Color bgColor = const Color.fromRGBO(7, 121, 148, 1);
+  static Color dangerous = const Color.fromRGBO(234, 11, 11, 1);
 }
