2a47a578a2271fbc66193db06f50c8a9e77d280f
Kilobe0 <104333874+Kilobe0@users.noreply.github.com>
2023-11-22 16:12:51 -0300

    Fixing dart lints

--- pork_pockets_app/lib/main.dart
+++ pork_pockets_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:pork_pockets_app/pages/cadastrar_renda_page.dart';
 import 'package:pork_pockets_app/pages/guide_page.dart';
@@ -7,7 +9,7 @@ import 'package:pork_pockets_app/pages/register_page.dart';
 main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
--- pork_pockets_app/lib/util/button_util.dart
+++ pork_pockets_app/lib/util/button_util.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pork_pockets_app/util/text_util.dart';
+import 'package:pork_pockets_app/util/color_util.dart';
 
-ElevatedButton SimpleButton(Function onPressed) 
-{
-  return ElevatedButton
-  (
+ElevatedButton SimpleButton(Function onPressed) {
+  return ElevatedButton(
       onPressed: () => onPressed,
-      style: ElevatedButton.styleFrom
-      (
+      style: ElevatedButton.styleFrom(
         backgroundColor: Paleta.azulEscurao,
       ),
       child: FormatedText("Criar conta", 24, FontWeight.bold));
