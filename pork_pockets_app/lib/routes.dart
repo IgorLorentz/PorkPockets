204127c1a21c78b29c9189c354cda1d7c1d10122
IgorLorentz <igorlorentz81@gmail.com>
2023-11-23 14:43:08 -0300

    Changing routes

--- pork_pockets_app/lib/routes.dart
+++ pork_pockets_app/lib/routes.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:pork_pockets_app/pages/guide_page.dart';
+import 'package:pork_pockets_app/pages/home_page.dart';
+import 'package:pork_pockets_app/pages/metas_page.dart';
+
 class Routes 
 {
-  static const HOME = '/';
-  static const LOGIN = '/login';
-  static const REGISTER = '/register';
-  static const METAS = '/metas';
-  static const NEW_METAS = '/new-metas';
+Map<String, WidgetBuilder> routes() 
+{
+  return 
+  {
+    '/': (context) => HomePage(),
+    '/guide-page': (context) => GuidePage(),
+    '/meta': (context) =>  MetasPage(),
+  };
+}
 }
