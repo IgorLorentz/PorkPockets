4f5a360ca1d9540f32cfa107b0359a84da8661b5
Kilobe0 <104333874+Kilobe0@users.noreply.github.com>
2023-11-26 16:01:28 -0300

    Fixed design

--- pork_pockets_app/lib/pages/home_page.dart
+++ pork_pockets_app/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: avoid_print, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:pork_pockets_app/pages/edit_meta_page.dart';
 import 'package:pork_pockets_app/util/appbar.dart';
 import 'package:pork_pockets_app/util/color_util.dart';
@@ -13,206 +14,183 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Paleta.bgColor,
       appBar: appBar(),
-      body: Column(children: [
-        SizedBox(
-          height: 30,
-        ),
-        Container(
-          padding: EdgeInsets.only(left: 19),
-          child: Row(
-            children: [
-              FormatedText('Olá User', 40, FontWeight.bold),
-            ],
-          ),
-        ),
-        SizedBox( height: 7),
-        Container(
-          padding: EdgeInsets.only(left: 24, right: 19),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              FormatedText('Seu salário:', 20, FontWeight.normal),
-              SizedBox(
-                width: 40,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Container(
+            width: 700,
+            child: Column(children: [
+              Row(children: [bigText('Olá User')]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FormatedText(
+                      'Seu salário: R\$ 1500,00', 20, FontWeight.normal),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  const EditMetaPage(), // TODO: Implement edit salario pop-up
+                            ));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Paleta.verde),
+                      child: const Icon(Icons.edit))
+                ],
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => EditMetaPage(),
-                        ));
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Paleta.verde,
-                  ),
-                  child: const Icon(Icons.edit))
-            ],
-          ),
-        ),
-        SizedBox( height: 17),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Paleta.azulEscurao,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Column(
-                    children: [
-                      smallText('Necessidades', FontWeight.bold,
-                          fontColor: Paleta.rosa),
-                      SvgPicture.asset('assets/images/50_porco.svg')
-                    ],
-                  ),
-                ),
-                mediumText("R\$ 1000,00")
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Paleta.azulEscurao,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Column(
-                    children: [
-                      smallText('Lazer', FontWeight.bold,
-                          fontColor: Paleta.rosa),
-                      SvgPicture.asset('assets/images/30_porco.svg')
-                    ],
-                  ),
-                ),
-                mediumText("R\$ 500,00")
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Paleta.azulEscurao,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Column(
-                    children: [
-                      smallText('Poupança', FontWeight.bold,
-                          fontColor: Paleta.rosa),
-                      SvgPicture.asset('assets/images/20_porco.svg')
-                    ],
-                  ),
-                ),
-                mediumText("R\$ 200,00")
-              ],
-            )
-          ],
-        ),
-        Container(
-          margin: EdgeInsets.all(15),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Paleta.azulEscuro),
-          child: Column(
-            children: [
+              const SizedBox(height: 30),
               Container(
-                padding: EdgeInsets.only(left: 20),
+                width: 600,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    FormatedText("Meta Atual: Moto", 20, FontWeight.normal),
-                    IconButton(
-                        onPressed: () {
-                          print("Favorite");
-                        },
-                        icon: Icon(Icons.favorite_outline)),
-                    IconButton(
-                        onPressed: () {
-                          print("Edit");
-                        },
-                        icon: Icon(Icons.edit)),
+                    card('Necessidades', '50', 'images/50_porco.png', 1000),
+                    card('Lazer', '30', 'images/30_porco.png', 300),
+                    card('Poupança', '20', 'images/20_porco.png', 200),
                   ],
                 ),
               ),
+              const SizedBox(height: 30),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-                width: double.infinity,
+                width: 700,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
-                    color: Colors.white),
-                child: Container(
-                    padding: EdgeInsets.only(left: 8, right: 8),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Paleta.verde),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [Text("Olá zé"), Text('R\$ 1000,00')],
-                    )),
+                    color: Paleta.azulEscuro),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          bigText("Meta Atual: Moto"),
+                          Row(
+                            children: [
+                              IconButton(
+                                  onPressed: () {
+                                    print("Edit");
+                                  },
+                                  icon: const Icon(Icons.edit)),
+                              IconButton(
+                                  onPressed: () {
+                                    print("Favorite");
+                                  },
+                                  icon: const Icon(Icons.favorite_outline)),
+                            ],
+                          )
+                        ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.white),
+                        child: Container(
+                            padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Paleta.verde),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                mediumText("R\$ 734,85",
+                                    fontColor: Colors.black),
+                                mediumText('R\$ 1000,00',
+                                    fontColor: Colors.black)
+                              ],
+                            )),
+                      ),
+                      const SizedBox(height: 10),
+                      IconButton(
+                          onPressed: () {
+                            print("Open");
+                          },
+                          icon: const Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Colors.black,
+                          )),
+                    ],
+                  ),
+                ),
               ),
-              IconButton(
-                  onPressed: () {
-                    print("Open");
-                  },
-                  icon: Icon(Icons.keyboard_arrow_down)),
-            ],
-          ),
-        ),
-        Column(
-          children: [
-            bigText('Reserva'),
-          ],
-        ),
-      ]),
-      drawer: SafeArea(
-        child: Drawer(
-          child: ListView(
-            children: [
-              const UserAccountsDrawerHeader(
-                accountName: Text("Seu nome e sobrenome"),
-                accountEmail: const Text("seuemail@mail.com"),
+              Column(
+                children: [
+                  bigText('Reserva'),
+                ],
               ),
-              ListTile(
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, "/anuncios");
-                },
-                leading: const Icon(Icons.announcement),
-                title: const Text("Anúncios"),
-                subtitle: const Text("mais informações..."),
-                trailing: const Icon(Icons.arrow_forward),
-              ),
-              ListTile(
-                  onTap: () {
-                    Navigator.pop(context);
-                    Navigator.pushNamed(context, "/guide-page");
-                  },
-                  leading: const Icon(Icons.info_outline),
-                  title: const Text("Guide"),
-                  subtitle: const Text("Guia do Usuário"),
-                  trailing: const Icon(Icons.arrow_forward)),
-              ListTile(
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, "/meta");
-                },
-                leading: const Icon(Icons.bar_chart_sharp),
-                title: const Text("Metas"),
-                subtitle: const Text("Suas metas"),
-                trailing: const Icon(Icons.arrow_forward),
-              ),
-            ],
+            ]),
           ),
         ),
       ),
+      drawer: drawer(context),
+    );
+  }
+
+  SafeArea drawer(BuildContext context) {
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Seu nome e sobrenome"),
+              accountEmail: Text("seuemail@mail.com"),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, "/anuncios");
+              },
+              leading: const Icon(Icons.announcement),
+              title: const Text("Anúncios"),
+              subtitle: const Text("mais informações..."),
+              trailing: const Icon(Icons.arrow_forward),
+            ),
+            ListTile(
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, "/guide-page");
+                },
+                leading: const Icon(Icons.info_outline),
+                title: const Text("Guide"),
+                subtitle: const Text("Guia do Usuário"),
+                trailing: const Icon(Icons.arrow_forward)),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, "/meta");
+              },
+              leading: const Icon(Icons.bar_chart_sharp),
+              title: const Text("Metas"),
+              subtitle: const Text("Suas metas"),
+              trailing: const Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column card(String title, String percent, String img, double value) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+          decoration: BoxDecoration(
+            color: Paleta.azulEscurao90,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            children: [
+              mediumText(title, fontColor: Paleta.rosa),
+              Image.asset(img, width: 150),
+              hugeText('$percent%', fontColor: Paleta.rosaClaro)
+            ],
+          ),
+        ),
+        mediumText("R\$ $value")
+      ],
     );
   }
 }
