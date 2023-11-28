f46fed18b6e797c9e15522ac13378506e476e454
IgorLorentz <igorlorentz81@gmail.com>
2023-11-28 17:39:20 -0300

    Update edit_meta

--- pork_pockets_app/lib/pages/edit_meta_page.dart
+++ pork_pockets_app/lib/pages/edit_meta_page.dart
@@ -23,87 +23,116 @@ class _EditMetaPageState extends State<EditMetaPage> {
         child: Column(
           children: [
             Container(
-              decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10)),
-                margin: const EdgeInsets.all(45),
-                child: Form(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 50),
-                        FormatedText("Editar Meta", 36, FontWeight.bold, fontColor: Paleta.azulEscurao),
-                        const SizedBox(height: 35),
-                        Row(
-                          children: [
-                            FormatedText("Insira um novo nome", 20, FontWeight.bold, fontColor: Paleta.azulEscurao),
-                          ],
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              margin: const EdgeInsets.all(45),
+              child: Form(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 50),
+                      FormatedText("Editar Meta", 36, FontWeight.bold,
+                          fontColor: Paleta.azulEscurao),
+                      const SizedBox(height: 35),
+                      Row(
+                        children: [
+                          FormatedText(
+                              "Insira um novo nome", 20, FontWeight.bold,
+                              fontColor: Paleta.azulEscurao),
+                        ],
+                      ),
+                      TextFormField(
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.number,
+                        obscureText: passwordObscured,
+                        decoration: const InputDecoration(
+                          hintText: "Nome da meta",
+                          border: OutlineInputBorder(borderSide: BorderSide()),
+                          fillColor: Color.fromARGB(200, 220, 220, 220),
+                          filled: true,
                         ),
-                        TextFormField(
-                          textInputAction: TextInputAction.next,
-                          keyboardType: TextInputType.number,
-                          obscureText: passwordObscured,
-                          decoration: const InputDecoration(
-                              hintText: "Valor",
-                              border:
-                                  OutlineInputBorder(borderSide: BorderSide()),
-                              fillColor: Color.fromARGB(200, 220, 220, 220),
-                              filled: true,
-                             ),
-                          style: const TextStyle(
-                            fontFamily: "Josefin",
-                            fontWeight: FontWeight.bold,
+                        style: const TextStyle(
+                          fontFamily: "Josefin",
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          FormatedText(
+                              "Insira um novo valor", 20, FontWeight.bold,
+                              fontColor: Paleta.azulEscurao),
+                        ],
+                      ),
+                      TextFormField(
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.number,
+                        obscureText: passwordObscured,
+                        decoration: const InputDecoration(
+                          hintText: "Valor",
+                          border: OutlineInputBorder(borderSide: BorderSide()),
+                          fillColor: Color.fromARGB(200, 220, 220, 220),
+                          filled: true,
+                        ),
+                        style: const TextStyle(
+                          fontFamily: "Josefin",
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Paleta.azulEscurao,
                           ),
-                        ),
-                        const SizedBox(height: 15),
-                        Row(
-                          children: [
-                            FormatedText("Insira um novo nome", 20, FontWeight.bold, fontColor: Paleta.azulEscurao),
-                          ],
-                        ),
-                        TextFormField(
-                          textInputAction: TextInputAction.next,
-                          keyboardType: TextInputType.number,
-                          obscureText: passwordObscured,
-                          decoration: const InputDecoration(
-                              hintText: "Valor",
-                              border:
-                                  OutlineInputBorder(borderSide: BorderSide()),
-                              fillColor: Color.fromARGB(200, 220, 220, 220),
-                              filled: true,
-                             ),
-                          style: const TextStyle(
-                            fontFamily: "Josefin",
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        ElevatedButton(
-                            onPressed: () {
-                              print("Botão clicado");
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Paleta.azulEscurao,
-                            ),
-                            child: FormatedText("Finalizar", 24, FontWeight.bold)),
-                        const SizedBox(height: 15),
-                        
-                      ],
-                    ),
+                          child:
+                              FormatedText("Finalizar", 24, FontWeight.bold)),
+                      const SizedBox(height: 15),
+                    ],
                   ),
                 ),
               ),
-              ElevatedButton(
-                        onPressed: () {
-                          print("Botão clicado");
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Paleta.dangerous,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (inContext) {
+                      return WillPopScope(
+                        onWillPop: () async => false,
+                        child: AlertDialog(
+                          title: Text('Deseja mesmo continuar?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(inContext);
+                              },
+                              child: Text('Cancelar'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(inContext);
+                              },
+                              child: Text('OK'),
+                            ),
+                          ],
                         ),
-                        child: FormatedText("Excluir", 24, FontWeight.bold)),
+                      );
+                    },
+                  );
+
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Paleta.dangerous,
+                ),
+                child: FormatedText("Excluir", 24, FontWeight.bold)),
           ],
         ),
       ),
-        
-      );    
+    );
   }
 }
