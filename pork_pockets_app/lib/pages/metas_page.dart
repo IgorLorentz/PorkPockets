f5739fa0be0decab8b88b78708e7d20757d7cb1c
IgorLorentz <igorlorentz81@gmail.com>
2023-11-30 19:44:36 -0300

    Update metas_page

--- pork_pockets_app/lib/pages/metas_page.dart
+++ pork_pockets_app/lib/pages/metas_page.dart
@@ -1,112 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pork_pockets_app/util/appbar.dart';
 import 'package:pork_pockets_app/util/color_util.dart';
 import 'package:pork_pockets_app/util/text_util.dart';
 
-class MetasPage extends StatefulWidget 
-{
+class MetasPage extends StatefulWidget {
   const MetasPage({super.key});
 
   @override
   State<MetasPage> createState() => _MetasPageState();
 }
 
-class _MetasPageState extends State<MetasPage> 
-{
+class _MetasPageState extends State<MetasPage> {
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold
-    (
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Paleta.bgColor,
-
-      appBar: AppBar
-      (
-        title: const Text("Layout Modelo"),
-        centerTitle: true,
-      ),
-
-      body: ListView
-      (
-        children: 
-        [
-          Column
-          (
+      appBar: appBar(),
+      body: ListView(
+        children: [
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
-
-            children: 
-            [
-              Container
-              (
+            children: [
+              Container(
                 width: double.infinity,
                 height: 40,
                 margin: EdgeInsets.only(left: 20, top: 35, bottom: 20),
-  
                 child: FormatedText("Suas metas:", 40, FontWeight.bold),
               ),
-
-              Container
-              (
+              Container(
                 margin: EdgeInsets.only(right: 30, bottom: 5),
                 alignment: Alignment.centerRight,
-
-                child: ElevatedButton
-                (
-                  onPressed: (){print("Cliquei");},
-                  
-                  style: ElevatedButton.styleFrom
-                  (
-                    backgroundColor: Paleta.azulEscurao
-                  ),
-
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/nova-meta");
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Paleta.azulEscurao),
                   child: FormatedText("Nova meta", 15, FontWeight.bold),
                 ),
               ),
-
-              Container
-              (
+              Container(
                 margin: EdgeInsets.all(15),
-                
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: Paleta.azulEscuro),
-
-                child: Column
-                (
-                  children: 
-                  [
-                    Row
-                    (
-                      children: 
-                      [
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Paleta.azulEscuro),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
                         FormatedText("Meta: Moto", 20, FontWeight.normal),
-                        IconButton(onPressed: (){favorite();}, icon: Icon(Icons.favorite_outline), color: Colors.black),
-                        IconButton(onPressed: (){print("Edit");}, icon: Icon(Icons.edit), color: Colors.black),
+                        IconButton(
+                            onPressed: () {
+                              favorite();
+                            },
+                            icon: Icon(Icons.favorite_outline),
+                            color: Colors.black),
+                        IconButton(
+                            onPressed: () {
+                              print("Edit");
+                            },
+                            icon: Icon(Icons.edit),
+                            color: Colors.black),
                       ],
                     ),
-
-                    Container
-                    (
+                    Container(
                       margin: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
                       width: double.infinity,
-                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: Colors.white),
-                      child: Container
-                      (
-                        padding: EdgeInsets.symmetric(vertical: 3, horizontal: 8),
-
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: Paleta.verde),
-                        child: Row
-                        (
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                          children: 
-                          [
-                            FormatedText("R\$ 0", 14, FontWeight.normal, fontColor: Colors.black),
-                            FormatedText("R\$ 12.000", 14, FontWeight.normal, fontColor: Colors.black)
-                          ],
-                        )
-                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white),
+                      child: Container(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 3, horizontal: 8),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Paleta.verde),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              FormatedText("R\$ 0", 14, FontWeight.normal,
+                                  fontColor: Colors.black),
+                              FormatedText("R\$ 12.000", 14, FontWeight.normal,
+                                  fontColor: Colors.black)
+                            ],
+                          )),
                     ),
-
-                    IconButton(onPressed: (){print("Open");}, icon: Icon(Icons.keyboard_arrow_down), color: Colors.black),
+                    IconButton(
+                        onPressed: () {
+                          print("Open");
+                        },
+                        icon: Icon(Icons.keyboard_arrow_down),
+                        color: Colors.black),
                   ],
                 ),
               ),
@@ -117,8 +102,7 @@ class _MetasPageState extends State<MetasPage>
     );
   }
 
-  void favorite()
-  {
+  void favorite() {
     print("Favorite");
   }
 }
