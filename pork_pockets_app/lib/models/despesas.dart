52ce7f9be62e87052c12f290b850ab932139e1b5
IgorLorentz <igorlorentz81@gmail.com>
2023-12-01 17:40:32 -0300

    Create despesas

---
+++ pork_pockets_app/lib/models/despesas.dart
@@ -0,0 +1,14 @@
+class Despesas{
+  String nome;
+  double valor;
+
+Despesas({
+  required this.nome,
+  required this.valor,
+});
+
+String getNome(){
+  return this.nome;
+}
+
+}
---
+++ pork_pockets_app/lib/repositories/despesas_repository.dart
@@ -0,0 +1,10 @@
+import 'package:pork_pockets_app/models/despesas.dart';
+
+class DespesasRepository{
+
+  static List<Despesas> tabela = [
+    Despesas(nome: 'Luz', valor: 200),
+    Despesas(nome: 'Água', valor: 150),
+  ];
+
+}
