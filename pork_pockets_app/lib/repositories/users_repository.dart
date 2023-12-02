35c887606042983ff69b5cefb91a166fa23ed04a
Mannelz <emannuelm29@gmail.com>
2023-12-02 15:55:57 -0300

    update user

---
+++ pork_pockets_app/lib/repositories/users_repository.dart
@@ -0,0 +1,18 @@
+import 'package:pork_pockets_app/models/pessoa.dart';
+
+class UsersRepository
+{
+  List<Pessoa> user = 
+  [
+    Pessoa(nome: 'Cleiton', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Ana', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Zakarias', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Gabriel', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Kleber', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Leandro', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Mario', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Paulo', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Janaina', email: 'cleiton@gmail.com', senha: '1234'),
+    Pessoa(nome: 'Scar', email: 'cleiton@gmail.com', senha: '1234'),
+  ];
+}
